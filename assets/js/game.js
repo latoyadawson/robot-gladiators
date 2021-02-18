25da38cbb58833de2a7fcb8db019e5b5f9e80402
Latoya Dawson <lmdw13@yahoo.com>
2021-02-17 22:13:32 -0800

    add enemys

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,15 +3,20 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-console.log(playerName, playerAttack, playerHealth, playerMoney);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto" , "Amy Android" , "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
 
+
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots
+//    * Fight all enemy-robots
+//    * Defeat each enemy-robot
+// "LOSE" - Player robot's health is zero or less
+
 //fight function 
-var fight = function() {
+var fight = function(enemyName) {
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 
@@ -72,4 +77,8 @@ var fight = function() {
 };
 
 // run fight function to start game
-fight();
+
+
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
